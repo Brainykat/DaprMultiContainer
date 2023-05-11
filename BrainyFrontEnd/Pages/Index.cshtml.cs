bf5a0d26e56d5b4bc55c049ed1af7cdcad99dc65
Chris <eng.christhuku@gmail.com>
2023-05-11 08:16:57 +0300

    Add project files.

---
+++ BrainyFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using BrainyFrontEnd.Models;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BrainyFrontEnd.Pages
+{
+  public class IndexModel : PageModel
+  {
+    private readonly ILogger<IndexModel> _logger;
+    private readonly DaprClient _daprClient;
+    public IndexModel(ILogger<IndexModel> logger, DaprClient daprClient)
+    {
+      _logger = logger;
+      _daprClient = daprClient;
+    }
+
+    public async Task OnGet()
+    {
+      var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
+            HttpMethod.Get,
+            "BrainyBackEnd",
+            "weatherforecast");
+
+      ViewData["WeatherForecastData"] = forecasts;
+    }
+  }
+}
